87d6dbfd515bd40236a8da676f80bd701113ea89
Aurelie <agauthier@lcgsolution.com>
2023-10-18 10:17:44 -0400

    projet final

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ function About() {
     <div>
       <h1>À propos de moi</h1>
       <p>Je m'appelle Aurélie, j'ai actuellement 23 ans et je poursuis une formation en programmation au Collège LaSalle.</p>
-      <p>Depuis mon plus jeune âge, j'ai développé une passion pour le monde de la technologie et de l'informatique, ce qui m'a naturellement conduit à me lancer dans des études en programmation. En dehors de ma formation, je suis une personne curieuse qui aime explorer de nouvelles idées et concepts dans le domaine de la programmation.</p> 
+      <p>Depuis mon plus jeune âge, j'ai développé une passion pour le monde de la technologie et de l'informatique, ce qui m'a naturellement conduit à me lancer dans des études en programmation. </p> 
       <p>Lorsque je ne suis pas plongé dans le code, mes passe-temps incluent la lecture, les jeux vidéo et l'écoute de la télévision. La lecture me permet de m'évader dans des mondes imaginaires, les jeux vidéo sont un moyen pour moi de relever des défis virtuels, et écouter la télévision me permet de me détendre après une journée de codage intense. </p>
       <p>J'aime également résoudre des Sudoku pour stimuler mon esprit.</p>
     </div>
